68217b9f71ad940ffc6cea485e6fac3c84fefd7e
Dasha Dubovaya <dasha.dubovaya@equeo.de>
2024-08-24 22:09:39 +0200

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+//import './App.css'
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState('');
 
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('https://api.quotable.io/random');
+              setQuote(response.data.content);
+            } catch (error) {
+              console.error('Error fetching quote:', error);
+            }
+          };
+        
+          const handleNewQuote = () => {
+            fetchQuote();
+          };
+        
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <h1>Random Quote Generator</h1>
+        <p>{quote}</p>
+        <button onClick={handleNewQuote}>New Quote</button>
+      </header>
+    </div>
+  );
 }
 
 export default App
